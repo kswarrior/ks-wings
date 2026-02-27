ac11e9fb774837bf671bc8bfe310525de608a689
kswarrior <kswarriorgh+main@gmail.com>
2026-02-27 11:28:05 +0530

    Update Deployment.js

--- routes/Deployment.js
+++ routes/Deployment.js
@@ -226,12 +226,268 @@ const createContainer = async (req, res) => {
   }
 };
 
-// === Other routes unchanged ===
-const deleteContainer = async (req, res) => { /* original code unchanged */ };
-const redeployContainer = async (req, res) => { /* original code unchanged */ };
-const reinstallContainer = async (req, res) => { /* original code unchanged */ };
-const editContainer = async (req, res) => { /* original code unchanged */ };
-const getContainerState = async (req, res) => { /* original code unchanged */ };
+// === Other routes ===
+const deleteContainer = async (req, res) => {
+  const containerId = req.params.id;
+  const container = docker.getContainer(containerId);
+  try {
+    const containerInfo = await container.inspect();
+    if (!containerInfo.Name) {
+      return res.status(400).json({
+        message: "Container does not have the required names",
+        containerId: containerId,
+      });
+    }
+
+    const volumeId = containerInfo.Name.replace(/^\//, "");
+    if (!volumeId) {
+      return res.status(400).json({
+        message: "No volumeId found in container",
+        containerId: containerId,
+      });
+    }
+    const volumePath = path.join(__dirname, "../volumes", volumeId);
+
+    const volumeExists = await fs
+      .access(volumePath)
+      .then(() => true)
+      .catch(() => false);
+
+    if (containerInfo.State.Running) {
+      log.info(`Stopping container ${containerInfo.Id.substring(0, 12)}`);
+      await container.stop({ t: 10 });
+    }
+
+    await container.remove();
+
+    if (volumeExists) {
+      await fs.rm(volumePath, { recursive: true, force: true });
+    }
+
+    const states = await readStates();
+    delete states[volumeId];
+    await writeStates(states);
+
+    res.status(200).json({
+      message: "Container and volume removed successfully",
+      containerId: containerId,
+      volumeId: volumeId,
+    });
+  } catch (err) {
+    console.error("Delete container error:", err);
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const redeployContainer = async (req, res) => {
+  const { id } = req.params;
+  const container = docker.getContainer(id);
+  try {
+    const { Idd } = req.params;
+    const { Disk } = req.body;
+    await updateState(Idd, "INSTALLING", null, Disk || 0);
+    const containerInfo = await container.inspect();
+    if (containerInfo.State.Running) {
+      log.info(`Stopping container ${id}`);
+      await container.stop();
+    }
+    log.info(`Removing container ${id}`);
+    await container.remove();
+
+    const { Image, Id, Ports, Memory, Cpu, PortBindings, Env } = req.body;
+    const volumePath = path.join(__dirname, "../volumes", Id);
+    log.info(`Pulling image: ${Image}`);
+    try {
+      const stream = await docker.pull(Image);
+      await new Promise((resolve, reject) => {
+        docker.modem.followProgress(stream, (err, result) => {
+          if (err) {
+            return reject(new Error(`Failed to pull image: ${err.message}`));
+          }
+          log.info(`Image ${Image} pulled successfully.`);
+          resolve(result);
+        });
+      });
+    } catch (err) {
+      log.error(`Error pulling image ${Image}:`, err);
+      return res.status(500).json({ message: err.message });
+    }
+
+    const containerOptions = createContainerOptions(
+      {
+        Image,
+        Id,
+        Ports,
+        Memory,
+        Cpu,
+        PortBindings,
+        Env,
+      },
+      volumePath
+    );
+
+    const newContainer = await docker.createContainer(containerOptions);
+    await newContainer.start();
+    res.status(200).json({
+      message: "Container redeployed successfully",
+      containerId: newContainer.id,
+    });
+    await updateState(Idd, "READY", newContainer.id, Disk || 0);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const reinstallContainer = async (req, res) => {
+  const { id } = req.params;
+  const container = docker.getContainer(id);
+
+  try {
+    const { Idd } = req.params;
+    await updateState(Idd, "INSTALLING");
+    const containerInfo = await container.inspect();
+    if (containerInfo.State.Running) {
+      log.info(`Stopping container ${id}`);
+      await container.stop();
+    }
+    log.info(`Removing container ${id}`);
+    await container.remove();
+
+    const env2json = (env) =>
+      env.reduce((obj, item) => {
+        const [key, value] = item.split("=");
+        obj[key] = value;
+        return obj;
+      }, {});
+
+    const { Image, Id, Ports, Memory, Cpu, PortBindings, Env, imageData } =
+      req.body;
+    const volumePath = path.join(__dirname, "../volumes", Id);
+
+    log.info(`Pulling image: ${Image}`);
+    try {
+      const stream = await docker.pull(Image);
+      await new Promise((resolve, reject) => {
+        docker.modem.followProgress(stream, (err, result) => {
+          if (err) {
+            return reject(new Error(`Failed to pull image: ${err.message}`));
+          }
+          log.info(`Image ${Image} pulled successfully.`);
+          resolve(result);
+        });
+      });
+    } catch (err) {
+      log.error(`Error pulling image ${Image}:`, err);
+      return res.status(500).json({ message: err.message });
+    }
+
+    const containerOptions = createContainerOptions(
+      {
+        Image,
+        Id,
+        Ports,
+        Memory,
+        Cpu,
+        PortBindings,
+        Env,
+      },
+      volumePath
+    );
+
+    const newContainer = await docker.createContainer(containerOptions);
+    if (
+      imageData &&
+      imageData.Scripts &&
+      imageData.Scripts.Install &&
+      Array.isArray(imageData.Scripts.Install)
+    ) {
+      const dir = path.join(__dirname, "../volumes", Id);
+
+      await downloadInstallScripts(
+        imageData.Scripts.Install,
+        dir,
+        env2json(Env)
+      );
+
+      const variables = {
+        primaryPort: Object.values(PortBindings)[0][0].HostPort,
+        containerName: newContainer.id.substring(0, 12),
+        timestamp: new Date().toISOString(),
+        randomString: Math.random().toString(36).substring(7),
+      };
+      await replaceVariables(dir, variables);
+    }
+
+    await newContainer.start();
+    res.status(200).json({
+      message: "Container reinstalled successfully",
+      containerId: newContainer.id,
+    });
+    await updateState(Idd, "READY", newContainer.id);
+  } catch (err) {
+    log.error("Error reinstalling instance:", err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const editContainer = async (req, res) => {
+  const { id } = req.params;
+  const { Image, Memory, Cpu, VolumeId } = req.body;
+
+  try {
+    log.info(`Editing container: ${id}`);
+    const container = docker.getContainer(id);
+    const containerInfo = await container.inspect();
+    const existingConfig = containerInfo.Config;
+    const existingHostConfig = containerInfo.HostConfig;
+
+    const newContainerOptions = createContainerOptions(
+      {
+        Image: Image || existingConfig.Image,
+        Id: id,
+        Ports: existingConfig.ExposedPorts,
+        Memory: Memory || existingHostConfig.Memory / (1024 * 1024),
+        Cpu: Cpu || existingHostConfig.CpuCount,
+        PortBindings: existingHostConfig.PortBindings,
+        Env: existingConfig.Env,
+        Cmd: existingConfig.Cmd,
+      },
+      path.join(__dirname, "../volumes", VolumeId)
+    );
+
+    log.info(`Stopping container: ${id}`);
+    await container.stop();
+    log.info(`Removing container: ${id}`);
+    await container.remove();
+    log.info("Creating new container with updated configuration");
+    const newContainer = await docker.createContainer(newContainerOptions);
+    await newContainer.start();
+
+    log.info(`Edit completed! New container ID: ${newContainer.id}`);
+    res.status(200).json({
+      message: "Container edited successfully",
+      oldContainerId: id,
+      newContainerId: newContainer.id,
+    });
+  } catch (err) {
+    log.error(`Edit failed: ${err.message}`);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const getContainerState = async (req, res) => {
+  const { volumeId } = req.params;
+  try {
+    const states = await readStates();
+    const containerState = states[volumeId] || { state: "UNKNOWN" };
+    log.info(JSON.stringify(containerState));
+    res.status(200).json(containerState);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 router.post("/instances/create", createContainer);
 router.delete("/instances/:id", deleteContainer);
